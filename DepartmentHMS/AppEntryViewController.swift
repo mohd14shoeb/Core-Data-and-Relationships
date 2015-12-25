9c076616cc19654d315cfc433fd546b399e81d3a
Anish Parajuli <anish.parajuli555@gmail.com>
2015-12-26 00:23:00 +0545

    confusion in deleting relationships...cant figure out either use cascade or nullify in my case...

--- DepartmentHMS/AppEntryViewController.swift
+++ DepartmentHMS/AppEntryViewController.swift
@@ -15,7 +15,7 @@ class AppEntryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
     }
 
     override func didReceiveMemoryWarning() {
